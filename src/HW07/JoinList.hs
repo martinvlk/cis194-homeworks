0ab865569d2d27ad5624664d20b2d92aa91933df
Martin Vlk <martin@vlkk.cz>
2015-07-12 09:39:46 +0200

    Some progress with testing.

--- src/HW07/JoinList.hs
+++ src/HW07/JoinList.hs
@@ -1,8 +1,8 @@
 module HW07.JoinList where
 
 import HW07.Sized
-import HW07.Scrabble
-import HW07.Buffer
+--import HW07.Scrabble
+--import HW07.Buffer
 
 import Data.Monoid
 
@@ -26,6 +26,7 @@ instance Monoid Or where
   mempty = Or False
   Or a `mappend` Or b = Or $ a || b
 
+{-
 data JoinListBasic a = Empty
                      | Single a
                      | Append (JoinListBasic a) (JoinListBasic a)
@@ -34,6 +35,7 @@ jlbToList :: JoinListBasic a -> [a]
 jlbToList Empty = []
 jlbToList (Single a) = [a]
 jlbToList (Append l1 l2) = jlbToList l1 ++ jlbToList l2
+-}
 
 data JoinList m a = Empty
                   | Single m a
@@ -41,3 +43,29 @@ data JoinList m a = Empty
                   deriving (Eq, Show)
 
 -- ex1
+
+-- Append op for Joinlist - yields a new JoinList whose monoidal
+-- annotation is derived from those of the two arguments.
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+n1 +++ n2 = Append (tag n1 <> tag n2) n1 n2
+
+-- Gets the annotation at the root of a JoinList
+tag :: Monoid m => JoinList m a -> m
+tag Empty = mempty
+tag (Single m _) = m
+tag (Append m _ _) = m
+
+-- ex2
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ a b = Nothing
+
+(!!?) :: [a] -> Int -> Maybe a
+[] !!? _        = Nothing
+_ !!? i | i < 0 = Nothing
+(x:xs) !!? 0    = Just x
+(x:xs) !!? i    = xs !!? (i-1)
+
+jlToList :: JoinList m a -> [a]
+jlToList Empty = []
+jlToList (Single _ a) = [a]
+jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
---
+++ tests/MainT.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+module Main where
+
+import Data.Monoid
+import Control.Applicative ((<$>), (<*>), pure)
+
+import Test.HUnit
+import Test.Framework as TF (defaultMain, testGroup, Test)
+import Test.Framework.Providers.HUnit
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+import Test.QuickCheck
+
+import HW07.JoinList
+import HW07.Sized
+
+main :: IO ()
+main = TF.defaultMain tests
+
+tests :: [TF.Test]
+tests = [ testGroup "QuickCheck HW07" [
+              testProperty "indexJ" prop_indexJ
+              ],
+          testGroup "HUnit HW07" [
+            t1
+            ]
+        ]
+
+t1 = testCase "indexJ" (indexJ i jl @=? jlToList jl !!? i)
+  where i = 3
+        jl = Append (Size 0) (Single (Size 1) 1) (Append (Size 1) (Append (Size 1) Empty (Single (Size 2) 2)) (Single (Size 2) 5))
+
+prop_indexJ :: Int -> (JoinList Size Int) -> Bool
+prop_indexJ i jl = (indexJ i jl) == (jlToList jl !!? i)
+
+instance Arbitrary (JoinList Size Int) where
+  arbitrary = oneof [pure Empty,
+                     Single <$> arbitrary <*> arbitrary,
+                     Append <$> arbitrary <*> arbitrary <*> arbitrary]
+
+instance Arbitrary Size where
+    arbitrary = Size <$> arbitrary
